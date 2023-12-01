a543c5d24aeb7b6ebe0bb3330b222ddab78ded49
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2023-12-01 18:40:54 +0100

    Implement test2 of day1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use axum::{routing::get, Router, extract::Path};
+use axum::{routing::get, Router, extract::Path, http::StatusCode};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
-        .route("/1/:num1/:num2", get(day1));
+        .route("/1/*nums", get(day1));
 
     Ok(router.into())
 }
 
-async fn day1(Path((num1, num2)): Path<(u32, u32)>) -> String {
-    let x = num1 ^ num2;
-    format!("{}", x * x * x)
+macro_rules! fail_if {
+    ($cond: expr) => {
+        if $cond {
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+}
+
+async fn day1(Path(nums): Path<String>)
+              -> Result<String, StatusCode>
+{
+    // No neutral element for xor.
+    fail_if!(nums.is_empty());
+    let mut nums = nums.split('/').map_while(|n| n.parse::<u32>().ok());
+    let x = nums.next();
+    fail_if!(x.is_none());
+    let mut x = x.unwrap();
+    for n in nums {
+        x ^= n
+    }
+    Ok(format!("{}", x * x * x))
 }
