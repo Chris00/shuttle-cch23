bd154f504fa5ee7732a270b21a453a7b4d1751fe
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2023-12-01 18:57:20 +0100

    Simplify code

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,15 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     Ok(router.into())
 }
 
-macro_rules! fail_if {
-    ($cond: expr) => {
-        if $cond {
-            return Err(StatusCode::INTERNAL_SERVER_ERROR);
-        }
-    };
-}
-
 async fn day1(Path(nums): Path<String>)
               -> Result<String, StatusCode>
 {
-    // No neutral element for xor.
-    fail_if!(nums.is_empty());
     let mut nums = nums.split('/').map_while(|n| n.parse::<u32>().ok());
+    // No neutral element for xor.
     let x = nums.next();
-    fail_if!(x.is_none());
+    if x.is_none() {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    }
     let mut x = x.unwrap();
     for n in nums {
         x ^= n
