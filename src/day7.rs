84b2c073cd2eed5e3fcc7b4ca3fd6d755701ec41
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2023-12-07 18:01:45 +0100

    Implement task 3 of day 7

--- src/day7.rs
+++ src/day7.rs
@@ -1,71 +1,75 @@
 use std::str;
-use axum::{http::StatusCode, Json};
+use axum::http::StatusCode;
 use axum_extra::extract::CookieJar;
 use base64::{Engine as _, engine::general_purpose};
-use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use eyre::eyre;
 
-type DynErr = Box<dyn std::error::Error>;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Quantities {
-    flour: u32,
-    sugar: u32,
-    butter: u32,
-    #[serde(rename = "baking powder")]
-    baking_powder: u32,
-    #[serde(rename = "chocolate chips")]
-    chocolate_chips: u32,
+fn compute_bake(cookie: CookieJar) -> eyre::Result<String> {
+    let recipe = decode_recipe(cookie)?;
+    let baking: Value = serde_json::from_str(&recipe)?;
+    let recipe;
+    if let Value::Object(r) = &baking["recipe"] {
+        recipe = r
+    } else {
+        return Err(eyre!("No 'recipe' field"))
+    }
+    let mut pantry;
+    if let Value::Object(p) = &baking["pantry"] {
+        pantry = p.clone()
+    } else {
+        return Err(eyre!("No 'pantry' field"))
+    }
+    let mut cookies = u64::MAX;
+    for (i, c) in recipe.iter() {
+        if let Value::Number(c) = c {
+            let c = c.as_u64();
+            if let Some(Value::Number(avail)) = pantry.get(i) {
+                let avail = avail.as_u64();
+                if let (Some(c), Some(avail)) = (c, avail) {
+                    cookies = cookies.min(avail / c)
+                } else {
+                    cookies = 0;
+                    break
+                }
+            } else {
+                // Ingredient not available or quantity not a number
+                cookies = 0;
+                break
+            }
+        }
+    }
+    if cookies > 0 {
+        // Subtract the ingredients used.
+        for (i, avail) in pantry.iter_mut() {
+            if let Value::Number(avail) = avail {
+                if let Some(Value::Number(c)) = recipe.get(i) {
+                    if let (Some(a), Some(c)) = (avail.as_u64(), c.as_u64()) {
+                        *avail = (a - cookies * c).into()
+                    }
+                }
+            }
+        }
+    }
+    Ok(format!(r#"{{ "cookies": {cookies}, "pantry": {} }}"#,
+        serde_json::to_string(&pantry)?))
 }
 
-#[derive(Deserialize, Debug)]
-struct Baking {
-    recipe: Quantities,
-    pantry: Quantities,
-}
-
-#[derive(Serialize, Debug)]
-pub struct Cookies {
-  cookies: u32,
-  pantry: Quantities,
-}
-
-fn compute_bake(cookie: CookieJar) -> Result<Cookies, DynErr> {
-    let (recipe, n) = decode_recipe(cookie)?;
-    let Json(baking): Json<Baking> = Json::from_bytes(&recipe[..n])?;
-    let recipe = baking.recipe;
-    let mut pantry = baking.pantry;
-    let cookies =
-        (pantry.flour / recipe.flour)
-            .min(pantry.sugar / recipe.sugar)
-            .min(pantry.butter / recipe.butter)
-            .min(pantry.baking_powder / recipe.baking_powder)
-            .min(pantry.chocolate_chips / recipe.chocolate_chips);
-    pantry.flour -= cookies * recipe.flour;
-    pantry.sugar -= cookies * recipe.sugar;
-    pantry.butter -= cookies * recipe.butter;
-    pantry.baking_powder -= cookies * recipe.baking_powder;
-    pantry.chocolate_chips -= cookies * recipe.chocolate_chips;
-    Ok(Cookies { cookies, pantry })
-}
-
-pub async fn bake(cookie: CookieJar) -> Result<Json<Cookies>, StatusCode> {
-    let c = compute_bake(cookie)
-        .or(Err(StatusCode::INTERNAL_SERVER_ERROR))?;
-    Ok(Json(c))
+pub async fn bake(cookie: CookieJar) -> Result<String, StatusCode> {
+    compute_bake(cookie)
+        .or(Err(StatusCode::INTERNAL_SERVER_ERROR))
 }
 
 pub async fn decode(cookie: CookieJar) -> Result<String, StatusCode> {
-    let (recipe, n) = decode_recipe(cookie)
-        .or(Err(StatusCode::INTERNAL_SERVER_ERROR))?;
-    Ok(str::from_utf8(&recipe[..n])
-        .or(Err(StatusCode::INTERNAL_SERVER_ERROR))?.to_string())
+    Ok(decode_recipe(cookie)
+        .or(Err(StatusCode::INTERNAL_SERVER_ERROR))?)
 }
 
-fn decode_recipe(cookie: CookieJar) -> Result<([u8; 256], usize), DynErr> {
+fn decode_recipe(cookie: CookieJar) -> eyre::Result<String> {
     let cookie = cookie.get("recipe")
-        .ok_or("Cookie 'recipe' not present")?
+        .ok_or(eyre!("Cookie 'recipe' not present"))?
         .value();
     let mut recipe = [0; 256];
     let n = general_purpose::STANDARD.decode_slice(cookie, &mut recipe)?;
-    Ok((recipe, n))
+    Ok(str::from_utf8(&recipe[..n])?.to_string())
 }
